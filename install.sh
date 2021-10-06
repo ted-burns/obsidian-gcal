23b53c4e78adb79d03841abf454272bd1ec92c9f
Teddy Burns <tburns@hubspot.com>
2021-10-05 21:32:26 -0400

    Add helloworld + install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R . ~/.obsidian/plugins/obsidian-gcal
---
+++ main.js
@@ -0,0 +1,8 @@
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+export default class GCalPlugin extends Plugin {
+  
+  async onload() {
+    console.log("hello world!")
+  }
+}
